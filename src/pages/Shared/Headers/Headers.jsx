c37be20838771e381c4c61a8357a5f970e9412ec
shahab17 <cse_1812020090@lus.ac.bd>
2023-10-31 11:33:24 +0600

    header section completed

--- src/pages/Shared/Headers/Headers.jsx
+++ src/pages/Shared/Headers/Headers.jsx
@@ -1,11 +1,16 @@
- 
+import moment from "moment";
+import logo from "../../../assets/logo.png";
 
 const Headers = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Headers</h2>
-        </div>
-    );
+  return (
+    <div className="text-center  mt-12">
+      <div className="flex justify-center mb-5">
+        <img src={logo} alt="" />
+      </div>
+      <p>Journalism Without Fear or Favour</p>
+      <p>{moment().format("dddd, MMMM D, YYYY")} </p>
+    </div>
+  );
 };
 
 export default Headers;
