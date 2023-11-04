651492ed5a330a45ab8f5ca07a29a651d8b84e8d
shahab17 <cse_1812020090@lus.ac.bd>
2023-11-04 20:26:05 +0600

    form completed

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,13 @@
  import PropTypes from 'prop-types'
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
 
     const {user, loading} = useContext(AuthContext)
+    const location = useLocation()
+    console.log(location.pathname)
 
     if(loading){
         return <div className='flex justify-center items-center h-[80vh] '><span className="loading loading-dots loading-lg"></span></div>
@@ -15,7 +17,7 @@ const PrivateRoute = ({children}) => {
         return children
     }
 
-    return <Navigate to='/login'></Navigate>
+    return <Navigate state={location.pathname} to='/login'></Navigate>
 };
 
 PrivateRoute.propTypes={
